620afe76f011112031f0dfdbb33f0ba180b7fcca
CarlosVirus <63506067+CarlosVirus@users.noreply.github.com>
2025-11-12 23:39:11 +0100

    Cosetes

---
+++ Assets/Scenes/Dialogues/Carlos.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Carlos : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public void FerUnaClasseDeMerda()
+    {
+        //el meu dia a dia
+        //tirar-li la canya al Xavi V.
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
